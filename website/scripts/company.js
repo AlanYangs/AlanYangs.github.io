1eee812a388c0a313bc4c7e37a21199f819d6888
AlanYang1 <alan.yang@software.dell.com>
2015-12-16 16:24:59 +0800

    clickon navigator

--- website/scripts/company.js
+++ website/scripts/company.js
@@ -6,3 +6,21 @@ function onTextFocused(){
 function onTextUnfoucsed(){
 	this.style.color = "#FFFFFF";
 }
+
+function switchNavigator(){
+	this.className="active_border";
+	this.getElementsByTagName("a").className="active_navigator";
+	var arrNavigators = document.getElementById("navigator").getElementsByTagName("div");
+	if(arrNavigators.length > 0){
+		alert(arrNavigators.length);
+		for(int i=0; i<arrNavigators.length; i++){
+			if(arrNavigators[i] != this){
+				arrNavigators[i].className = "";
+				arrNavigators[i].getElementsByTagName("a").className="";
+			}else{
+				this.className="active_border";
+				this.getElementsByTagName("a").className="active_navigator";
+			}
+		}
+	}
+}
